5678704e280ca4c3d32bd690cb8f20cd56104442
Lucas, Vance <vlucas@netsuite.com>
2019-06-13 11:07:04 -0500

    Base setup.sh script with shell aliases + tmux

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+[ ! -f ~/.shell.dotfiles ] && ln -s "$(pwd)/shell/.shell.dotfiles" ~/.shell.dotfiles
+[ ! -f ~/.tmux.conf ] && ln -s "$(pwd)/tmux/.tmux.conf" ~/.tmux.conf
+
+
+# Reference local ~/.shell.dotfiles in ~/.bashrc and/or ~/.zshrc
+[ -f ~/.bashrc ] && grep "source ~/.shell.dotfiles" ~/.bashrc -q || echo "source ~/.shell.dotfiles" >> ~/.bashrc
+[ -f ~/.zshrc ] && grep "source ~/.shell.dotfiles" ~/.zshrc -q || echo "source ~/.shell.dotfiles" >> ~/.zshrc
